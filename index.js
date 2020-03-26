5c164a08b06561fa463d54f33ecd537e338b4cce
Brian Russell <bruss@brians-mbp.lan>
2020-03-26 15:14:11 -0400

    Completed Task 1

--- index.js
+++ index.js
@@ -41,8 +41,26 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  
+  eat(someFood){
+    if (this.stomach.length < 10){
+      this.stomach.push(someFood);
+    } //closes "if" statement
+  }
 
-}
+  poop(){
+    this.stomach = [];
+  } // closes poop method
+
+  toString(){
+    return `${this.name}, ${this.age}`
+  }
+} // closes Person
 
 /*
   TASK 2
